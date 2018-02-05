2000e2d4721e22eaffbb610a791fd7715eeb852d
Tinker-zzz <zgt_youxaing@163.com>
2018-02-05 17:52:07 +0800

    temp

---
+++ www/static/orm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Tinker'
+
+import logging
+import asyncio
+import aiomysql
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+
